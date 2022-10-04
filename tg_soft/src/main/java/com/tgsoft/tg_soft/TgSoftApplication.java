b347b05aa4584ea3d5ad4c2f7576934361a4f91e
genysonbruno <genysonbruno@gmail.com>
2022-10-04 10:44:08 -0300

    inicio do projeto com spring

---
+++ tg_soft/src/main/java/com/tgsoft/tg_soft/TgSoftApplication.java
@@ -0,0 +1,13 @@
+package com.tgsoft.tg_soft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgSoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgSoftApplication.class, args);
+	}
+
+}
---
+++ tg_soft/src/test/java/com/tgsoft/tg_soft/TgSoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgsoft.tg_soft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgSoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
